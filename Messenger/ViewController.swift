3628b3f060368f7411dc821283bfe7b8d9b73383
Phillip Cabaniss <phillipcabaniss@phillips-mbp.arl.nam.gm.com>
2020-06-09 09:16:21 -0500

    Getting Started

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Phillip Cabaniss on 6/9/20.
+//  Copyright © 2020 Phillip Cabaniss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
